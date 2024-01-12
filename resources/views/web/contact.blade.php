d60a081099ea287fb1511d47f1f913369c1eb7c8
Cybernetics <mail@cybernetics.me>
2024-01-12 12:35:18 +0530

    small updates

--- resources/views/web/contact.blade.php
+++ resources/views/web/contact.blade.php
@@ -57,9 +57,18 @@
                 <div class="col-lg-6">
                     <div class="appear-animation animated fadeIn appear-animation-visible" data-appear-animation="fadeIn" data-appear-animation-delay="800" style="animation-delay: 800ms;">
                         <ul class="list list-icons list-icons-style-2 mt-2">
-                            <li><i class="fas fa-phone top-6"></i> <strong class="text-dark">Phone:</strong> <a href="callto:+919562417322">+91 9562417322</a></li>
-                            <li><i class="fas fa-phone top-6"></i> <strong class="text-dark">Phone:</strong> <a href="callto:+919048550123">+91 9048550123</a></li>
+                            <li><i class="fas fa-map-marker-alt top-6"></i> <strong class="text-dark">Address:</strong> Habilete Learning Solutions Pvt. Ltd, Near Iron Bridge, High School Junction, Kollam, Kerala, India, <a href="https://habilete.co.in" target="_blank">www.habilete.co.in</a></li>
+                            <li><i class="fas fa-phone top-6"></i> <strong class="text-dark">Mobile:</strong> <a href="callto:+919562417322">+91 9562417322</a></li>
+                            <li><i class="fas fa-phone top-6"></i> <strong class="text-dark">Mobile:</strong> <a href="callto:+919048550123">+91 9048550123</a></li>
+                            <li><i class="fas fa-phone top-6"></i> <strong class="text-dark">Phone:</strong> <a href="callto:+914742796123">+91 474 2796123</a></li>
                             <li><i class="fas fa-envelope top-6"></i> <strong class="text-dark">Email:</strong> <a href="mailto:iccesp2024@gmail.com">iccesp2024@gmail.com</a></li>
+                        </ul>
+                        <ul class="list list-icons list-icons-style-2 mt-2">
+                            <li><i class="fas fa-map-marker-alt top-6"></i> <strong class="text-dark">Address:</strong> Department of Civil Engineering, Marian Engineering College, Kazhakuttom,Thiruvananthapuiram-695582, <a href="https://marian.ac.in" target="_blank">www.marian.ac.in</a></li>
+                            <li><i class="fas fa-phone top-6"></i> <strong class="text-dark">Prof. Abhijith R.P:</strong> <a href="callto:+919567687372 ">+91 9567687372 </a></li>
+                            <li><i class="fas fa-envelope top-6"></i> <strong class="text-dark">Email:</strong> <a href="mailto:abhijithrp.ce@marian.ac.in">abhijithrp.ce@marian.ac.in</a></li>
+                            <li><i class="fas fa-phone top-6"></i> <strong class="text-dark">Prof Renju C.M:</strong> <a href="callto:+919400456728 ">+91 9400456728 </a></li>
+                            <li><i class="fas fa-envelope top-6"></i> <strong class="text-dark">Email:</strong> <a href="mailto:renjucm.ce@marian.ac.in">renjucm.ce@marian.ac.in</a></li>
                         </ul>
                     </div>
                 </div>
