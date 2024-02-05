43cc7e31ff68fd8de645d7afbf8a431982720ea9
Cybernetics <mail@cybernetics.me>
2024-02-05 14:37:32 +0530

    small updates

--- app/Mail/AbstractSubmissionEmail.php
+++ app/Mail/AbstractSubmissionEmail.php
@@ -54,7 +54,7 @@ class AbstractSubmissionEmail extends Mailable
     public function attachments(): array
     {
         return [
-            Attachment::fromStorage($this->abstract->doc_url)->withMime($this->mime),
+            Attachment::fromPath($this->abstract->doc_url)->withMime($this->mime),
         ];
     }
 }
