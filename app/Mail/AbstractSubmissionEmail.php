cdb993517a73bdf09f49bcff87b6cde0588d9954
Cybernetics <mail@cybernetics.me>
2024-02-05 14:21:28 +0530

    small updates

--- app/Mail/AbstractSubmissionEmail.php
+++ app/Mail/AbstractSubmissionEmail.php
@@ -5,6 +5,7 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Attachment;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
@@ -51,6 +52,8 @@ class AbstractSubmissionEmail extends Mailable
      */
     public function attachments(): array
     {
-        return [];
+        return [
+            Attachment::fromStorage($this->abstract->doc_url)->withMime('application/pdf'),
+        ];
     }
 }
